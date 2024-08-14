cc191ca5b5e6a74a84e98d9634c1113c17376b8a
Pushkaran_Tyagi163 <117625210+Pushkaran163@users.noreply.github.com>
2024-08-15 02:04:21 +0530

    add file

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection{
     private static final String URL = "jdbc:mysql://localhost:3306/pharmacy_management";
     private static final String USER = "root";
-    private static final String PASSWORD = "Sayanyellow12!_";
+    private static final String PASSWORD = "enter your password here";
 
     public static Connection getConnection(){
         try{
